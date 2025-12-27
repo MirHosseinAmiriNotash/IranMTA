c1147f99c9d1cbe985a65a577e77fa5eacc552f4
Hossein <mh.amiri.notash@gmail.com>
2025-12-27 21:39:26 +0330

    remove unused files

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ function App() {
       style={{
         display: "flex",
         flexDirection: "column",
-        overflowY: "auto", 
+        overflowY: "auto",
         overflowX: "hidden",
       }}
     >
